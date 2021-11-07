a8ac068e1c04b90da074f576287032c3f03ea52f
Baklaikin Kostiantyn <baklaikin@gmail.com>
2021-11-07 17:10:22 +0200

    fixed positioning

--- src/views/Homepage/Homepage.styled.jsx
+++ src/views/Homepage/Homepage.styled.jsx
@@ -2,6 +2,6 @@ import styled from "@emotion/styled";
 
 export const List = styled.ul`
   display: flex;
-  justify-content: space-evenly;
-  margin-top: 30px;
+  justify-content: center;
+  margin-top: 150px;
 `;
