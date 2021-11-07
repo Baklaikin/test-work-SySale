0f4d55ba78ce2678aec88e523a5549650a447e43
Baklaikin Kostiantyn <baklaikin@gmail.com>
2021-11-07 17:06:23 +0200

    fixed some text

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import "./App.css";
 import Homepage from "views/Homepage/Homepage";
 
 function App() {
-  return (
-    <>
-      <h1>Let's go</h1>
-      <Homepage />
-    </>
-  );
+  return <Homepage />;
 }
 
 export default App;
